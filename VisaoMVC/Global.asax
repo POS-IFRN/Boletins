d05afd6c46196cb7b9f5d5dbea9ec232d372ab5f
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2016-05-14 22:46:22 -0300

    criacao da VisaoMVC

---
+++ VisaoMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VisaoMVC.MvcApplication" Language="C#" %>
---
+++ VisaoMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VisaoMVC.Startup))]
+namespace VisaoMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
